2122ac6d3b5787a1c0c77273bdf2fa163236d647
imytdotio <imytdotio@gmail.com>
2023-01-26 17:19:26 +0800

    minor change tune

--- src/Componenets/Account.js
+++ src/Componenets/Account.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { supabase } from "../Config/supabase";
 import { AuthContext } from "../Context/AuthContext";
+import { Auth } from "./Auth";
 import { Pill } from "./Components";
 
 /**
@@ -16,14 +17,20 @@ export const Account = (props) => {
     .filter((arr) => {
       return arr[1] === true;
     });
-  console.log(array);
 
   return (
     <>
-      <h1 className="">{profile.nickname}</h1>
-      {array.map((skill) => {
-        return <Pill>{skill[0]}</Pill>;
-      })}
+      {isAuthenticated ? (
+        <>
+          <h1 className="">{profile.nickname}</h1>
+          {array.map((skill) => {
+            return <Pill>{skill[0]}</Pill>;
+          })}
+          <Auth />
+        </>
+      ) : (
+        <Auth />
+      )}
     </>
   );
 };
